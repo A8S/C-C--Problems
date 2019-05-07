a140189f8f1165146b7915a2769102ec1a65338a
A8S <33037679+A8S@users.noreply.github.com>
2019-05-07 23:01:57 +0530

    Add files via upload

---
+++ js_loops_problems.js
@@ -0,0 +1,14 @@
+//print odd number from 45 to 100 using for
+var i;
+for(i=0;i<20;i++)
+    {
+    console.log(i)
+    }
+//factorial
+var y=6;
+var fact=1;
+for(i=1;i<=y;i=i+1)
+{
+fact=fact*i
+}
+console.log(fact);
